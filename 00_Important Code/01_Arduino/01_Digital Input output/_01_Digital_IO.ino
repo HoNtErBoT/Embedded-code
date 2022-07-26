ebbf0e4dc0f5e706ad03935d3dba54cfa681385c
Rajeev TR <109785046+HoNtErBoT@users.noreply.github.com>
2022-07-26 07:59:45 +0530

    Add files via upload

---
+++ 00_Important Code/01_Arduino/01_Digital Input output/_01_Digital_IO.ino
@@ -0,0 +1,19 @@
+void setup() 
+{
+ pinMode(13,OUTPUT);
+ pinMode(2,INPUT);
+}
+
+void loop()
+{
+  if(digitalRead(2)==1)
+  {
+    digitalWrite(13,HIGH);
+    delay(50);
+  }
+  else
+  {
+    digitalWrite(13,LOW);
+    delay(50);
+  }
+}
